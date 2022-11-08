9ccb21de257d12d5f18a4422fda9da100dcee885
nicolas.marin <nicolas.marin@berger-levrault.com>
2022-11-08 22:55:44 +0100

    change cors policy

--- spring/springconfig/src/main/java/com/lagrange/springconfig/config/Config.java
+++ spring/springconfig/src/main/java/com/lagrange/springconfig/config/Config.java
@@ -58,7 +58,7 @@ public class Config  {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**").allowedOrigins("http://localhost:4200")
+                registry.addMapping("/**").allowedOrigins("http://localhost:8080")
                     .allowedMethods("PUT", "DELETE", "GET", "POST");
             }
         };
