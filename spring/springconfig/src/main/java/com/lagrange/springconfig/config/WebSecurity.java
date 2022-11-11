082b7ee217b42cd28de6e8d1a11bb80cd08d166a
Nicolas Marin <marin.nicolas.ludovic@gmail.com>
2022-11-11 15:59:02 +0100

    modify cors

--- spring/springconfig/src/main/java/com/lagrange/springconfig/config/WebSecurity.java
+++ spring/springconfig/src/main/java/com/lagrange/springconfig/config/WebSecurity.java
@@ -4,6 +4,8 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.List;
 
@@ -11,15 +13,6 @@ import java.util.List;
 public class WebSecurity extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        CorsConfiguration corsConfiguration = new CorsConfiguration();
-        corsConfiguration.setAllowedHeaders(List.of("Authorization", "Cache-Control", "Content-Type"));
-        corsConfiguration.setAllowedOrigins(List.of("*"));
-        corsConfiguration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PUT","OPTIONS","PATCH", "DELETE"));
-        corsConfiguration.setExposedHeaders(List.of("Authorization"));
-
-        // You can customize the following part based on your project, it's only a sample
-        http.authorizeRequests().antMatchers("/**").permitAll().anyRequest()
-            .authenticated().and().csrf().disable().cors().configurationSource(request -> corsConfiguration);
-
+        http.cors().configurationSource(request -> new CorsConfiguration().applyPermitDefaultValues());
     }
 }
