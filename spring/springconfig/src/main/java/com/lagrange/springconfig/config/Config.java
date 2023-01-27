a2744394c8ce6d853fb287f0c28545efe9b6245e
Nicolas Marin <marin.nicolas.ludovic@gmail.com>
2023-01-27 15:14:51 +0100

    remove last bad

--- spring/springconfig/src/main/java/com/lagrange/springconfig/config/Config.java
+++ spring/springconfig/src/main/java/com/lagrange/springconfig/config/Config.java
@@ -18,12 +18,11 @@ import org.springframework.boot.autoconfigure.AutoConfigurationPackage;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @Configuration
 @EnableScheduling
-@EnableJpaRepositories("")
+//@EnableJpaRepositories("")
 @EntityScan("com.lagrange.entity")
 public class Config  {
 
